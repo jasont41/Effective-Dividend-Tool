a8d46443db988119903938efd680050bde307cac
Jason Eastman <jasont41@gmail.com>
2021-01-12 13:47:46 -0800

    Update main.cpp

--- Effective Dividend Tool/main.cpp
+++ Effective Dividend Tool/main.cpp
@@ -9,24 +9,26 @@
 #include <iomanip>
 using namespace std;
 float calculateEffective(float annualDividend,float currentShares,int purchasedShares,int price){
-    return ((annualDividend *(purchasedShares+currentShares))/price);
+    return (currentShares*annualDividend/price);
 }
 
 int main(int argc, const char * argv[]) {
-    float annualDividend, currentShares, effectiveDividend, price;
-    int months = 120,purchasedShares = 50 ;
-    currentShares = purchasedShares = 50;
+    float annualDividend, currentShares, effectiveDividend, price,ogYield;
+    int months = 360,purchasedShares = 100;
+    currentShares = 100;
     annualDividend = 0.235 * 12;
+    ogYield = annualDividend;
     price = 57.50;
     effectiveDividend = annualDividend/price;
+    cout << effectiveDividend << endl;
     cout << "Month\t Effective Yield\t Monthly Dividend\tCurrent Shares" << endl;
     for(int i = 0; i < months; i++){
        if(i%12 ==  0){
             annualDividend += annualDividend * 0.0045;
         }
-        float tempDiv  = annualDividend/12 * currentShares;
+        float tempDiv  = (annualDividend/12) * currentShares;
         currentShares = currentShares + (tempDiv/price);
-        cout <<setprecision(4) <<  i+1 << "\t     " << calculateEffective(annualDividend, currentShares, currentShares, price) <<  "\t\t\t\t\t " << tempDiv << "\t\t\t" << currentShares << endl;
+        cout <<setprecision(4) <<  i+1 << "\t     " << calculateEffective(annualDividend, currentShares, purchasedShares, price) <<  "\t\t\t\t\t " << tempDiv << "\t\t\t" << currentShares << endl;
     }
     return 0;
 }
