923371d3fe4cd13f622292c89c7ae6464a10bfcb
Jason Eastman <jasont41@gmail.com>
2021-01-12 12:44:45 -0800

    Initial Commit

---
+++ Effective Dividend Tool/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Effective Dividend Tool
+//
+//  Created by jason eastman on 1/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
