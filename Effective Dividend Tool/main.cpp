95c5f792a1e6becb1b1ea55ca7509a260e361546
Jason Eastman <jasont41@gmail.com>
2021-01-13 23:06:38 -0800

    Update main.cpp

--- Effective Dividend Tool/main.cpp
+++ Effective Dividend Tool/main.cpp
@@ -8,27 +8,24 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-float calculateEffective(float annualDividend,float currentShares,int purchasedShares,int price){
-    return (currentShares*annualDividend/price);
+float calculateEffective(float annualDividend,float currentShares,int purchaseShares, int price){
+    return (annualDividend/price)*(currentShares/purchaseShares)*100;
 }
 
 int main(int argc, const char * argv[]) {
-    float annualDividend, currentShares, effectiveDividend, price,ogYield;
-    int months = 360,purchasedShares = 100;
-    currentShares = 100;
-    annualDividend = 0.235 * 12;
-    ogYield = annualDividend;
-    price = 57.50;
+    float annualDividend, currentShares, effectiveDividend, price;
+    int years = 40,purchasedShares;
+    purchasedShares = currentShares = 200;
+    annualDividend = 2.08;
+    price = 28.75;
     effectiveDividend = annualDividend/price;
-    cout << effectiveDividend << endl;
-    cout << "Month\t Effective Yield\t Monthly Dividend\tCurrent Shares" << endl;
-    for(int i = 0; i < months; i++){
-       if(i%12 ==  0){
-            annualDividend += annualDividend * 0.0045;
-        }
-        float tempDiv  = (annualDividend/12) * currentShares;
+    cout << "Year\t Effective Yield\t Annual Dividend\tCurrent Shares" << endl;
+    for(int i = 0; i < years; i++){
+        annualDividend += annualDividend * 0.002;
+        float tempDiv  = annualDividend * currentShares;
         currentShares = currentShares + (tempDiv/price);
-        cout <<setprecision(4) <<  i+1 << "\t     " << calculateEffective(annualDividend, currentShares, purchasedShares, price) <<  "\t\t\t\t\t " << tempDiv << "\t\t\t" << currentShares << endl;
+        cout <<setprecision(4) <<  i+1 << "\t     " << calculateEffective(annualDividend, currentShares, purchasedShares, price) <<  "%\t\t\t\t $" << tempDiv << "\t\t\t\t" << currentShares << endl;
     }
+    cout << setprecision(6) << "Purchase Price: " << purchasedShares * price << "\t\t\t\ Current Price: " << currentShares * price << endl;
     return 0;
 }
